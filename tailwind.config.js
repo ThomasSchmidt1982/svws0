17edf25ef9c48d2567dc5a8f6136dfff69081016
Thomas Schmidt <thomas.michel.schmidt@gmail.com>
2026-06-22 11:49:43 +0200

    scale

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
     content: ["./**/*.{html,js}"],
     theme: {
         extend: {
-            /* mettre personnalisation à ajouter ici */
+            fontFamily:{
+                sans: ['Inter', 'sans-serif']
+            }
         },
     },
     plugins: [],
