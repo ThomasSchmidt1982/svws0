e1e8f1b52590b8439b0a52df6b2b024e5be755d3
Thomas Schmidt <thomas.michel.schmidt@gmail.com>
2026-06-22 11:17:39 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        extend: {
+            /* mettre personnalisation à ajouter ici */
+        },
+    },
+    plugins: [],
+}
